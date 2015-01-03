8e7e2652075960318bcb66f0ce82c1b27eef8593
Vasya Novikov <n1dr+cm3513git@ya.ru>
2015-01-04 00:06:35 +0300

    split "rangom game" test into a separate file

--- modules/paper-engine/src/test/scala/net/pointsgame/paper_engine/FieldTest.scala
+++ modules/paper-engine/src/test/scala/net/pointsgame/paper_engine/FieldTest.scala
@@ -1,13 +1,9 @@
 package net.pointsgame.paper_engine
 
-import scala.util.{ Random, Try }
+import net.pointsgame.paper_engine.Images._
 import org.scalatest.{ DiagrammedAssertions, FunSuite }
-import org.scalatest.prop.Checkers
-import org.scalacheck._
-import org.scalacheck.Prop._
 
-class FieldTest extends FunSuite with DiagrammedAssertions with Checkers {
-  import Images._
+class FieldTest extends FunSuite with DiagrammedAssertions {
 
   test("simple surround") {
     constructLastFieldWithRotations(
@@ -247,7 +243,7 @@ class FieldTest extends FunSuite with DiagrammedAssertions with Checkers {
       }
   }
 
-  test("2 adjacent surroundings") {
+  test("2 surroundings with common border") {
     constructLastFieldWithRotations(
       """
       .a..
@@ -264,7 +260,7 @@ class FieldTest extends FunSuite with DiagrammedAssertions with Checkers {
       }
   }
 
-  test("2 opposite surroundings") {
+  test("2 surroundings with common dot") {
     constructLastFieldWithRotations(
       """
       .a.a.
@@ -280,7 +276,7 @@ class FieldTest extends FunSuite with DiagrammedAssertions with Checkers {
       }
   }
 
-  test("3 surroundings") {
+  test("3 surroundings with common borders") {
     constructLastFieldWithRotations(
       """
       ..a..
@@ -298,28 +294,4 @@ class FieldTest extends FunSuite with DiagrammedAssertions with Checkers {
       }
   }
 
-  val minSize = 3
-  val maxSize = 50
-  val lengthGen = Gen.choose(minSize, maxSize)
-  val seedGen = Arbitrary.arbitrary[Int]
-  test("random game")(check(Prop.forAll(lengthGen, lengthGen, seedGen) { (width: Int, height: Int, seed: Int) =>
-    val random = new Random(seed)
-    val moves = random.shuffle((0 until width * height).toVector).map(idx => Pos(idx % width, idx / width))
-    val field = Field(width, height)
-    val finalFieldTry = Try {
-      moves.foldLeft(field)((acc, pos) => if (acc.isPuttingAllowed(pos)) acc.putPoint(pos) else acc)
-    }
-    val propsTry = finalFieldTry.map { finalField =>
-      all(
-        (finalField.scoreRed >= 0) :| "red score should be non-negative",
-        (finalField.scoreBlack >= 0) :| "black score should be non-negative",
-        ((field.scoreRed - field.scoreBlack).abs < width * height / 2) :| "score difference should be less than number of player moves",
-        (field.scoreRed + field.scoreBlack <= (width - 2) * (height - 2)) :| "full score should be less than field size"
-      )
-    }
-    all(
-      finalFieldTry.isSuccess :| "should be no exceptions",
-      propsTry.get
-    )
-  }))
 }
---
+++ modules/paper-engine/src/test/scala/net/pointsgame/paper_engine/RandomGameFieldTest.scala
@@ -0,0 +1,35 @@
+package net.pointsgame.paper_engine
+
+import org.scalacheck.Prop._
+import org.scalacheck._
+import org.scalatest.prop.Checkers
+import org.scalatest.{ DiagrammedAssertions, FunSuite }
+import scala.util.{ Random, Try }
+
+class RandomGameFieldTest extends FunSuite with DiagrammedAssertions with Checkers {
+  val minSize = 3
+  val maxSize = 50
+  val lengthGen = Gen.choose(minSize, maxSize)
+  val seedGen = Arbitrary.arbitrary[Int]
+  test("random game")(check(Prop.forAll(lengthGen, lengthGen, seedGen) { (width: Int, height: Int, seed: Int) =>
+    val random = new Random(seed)
+    val moves = random.shuffle((0 until width * height).toVector).map(idx => Pos(idx % width, idx / width))
+    val field = Field(width, height)
+    val finalFieldTry = Try {
+      moves.foldLeft(field)((acc, pos) => if (acc.isPuttingAllowed(pos)) acc.putPoint(pos) else acc)
+    }
+    val propsTry = finalFieldTry.map { finalField =>
+      all(
+        (finalField.scoreRed >= 0) :| "red score should be non-negative",
+        (finalField.scoreBlack >= 0) :| "black score should be non-negative",
+        ((field.scoreRed - field.scoreBlack).abs < width * height / 2) :| "score difference should be less than number of player moves",
+        (field.scoreRed + field.scoreBlack <= (width - 2) * (height - 2)) :| "full score should be less than field size"
+      )
+    }
+    all(
+      finalFieldTry.isSuccess :| "should be no exceptions",
+      propsTry.get
+    )
+  }))
+
+}
