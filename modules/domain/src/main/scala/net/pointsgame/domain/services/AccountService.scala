10634261ec07506e517bf4320c8e73210b94fc89
Kurnevsky Evgeny <kurnevsky@gmail.com>
2015-09-02 23:06:51 +0300

    Small refactoring.

--- modules/domain/src/main/scala/net/pointsgame/domain/services/AccountService.scala
+++ modules/domain/src/main/scala/net/pointsgame/domain/services/AccountService.scala
@@ -2,8 +2,6 @@ package net.pointsgame.domain.services
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scalaz._
-import Scalaz._
 import com.github.nscala_time.time.Imports._
 import net.pointsgame.domain.model.User
 import net.pointsgame.domain.repositories.UserRepository
@@ -16,12 +14,14 @@ final case class AccountService(userRepository: UserRepository) {
       Future.failed(new DomainException("Name shouldn't be empty."))
     if (name.length > Constants.maxNameLength)
       Future.failed(new DomainException(s"Name length should be not more than ${Constants.maxNameLength}."))
-    val result = for (exists <- userRepository.existsWithName(name)) yield {
-      if (exists)
-        throw new DomainException(s"User with name $name already exists.")
-      val salt = Hasher.generateSalt(Constants.saltLength)
-      userRepository.insert(User(None, name, Hasher.hash(password, salt), salt, DateTime.now()))
-    }
-    result.join
+    for {
+      exists <- userRepository.existsWithName(name)
+      result <- if (exists) {
+        Future.failed(new DomainException(s"User with name $name already exists."))
+      } else {
+        val salt = Hasher.generateSalt(Constants.saltLength)
+        userRepository.insert(User(None, name, Hasher.hash(password, salt), salt, DateTime.now()))
+      }
+    } yield result
   }
 }
